d1e4c24ed4e85a0f7e540aa0162331d5419e38f3
Ting Wei Fan <tfan2437@gmail.com>
2025-04-30 22:46:07 +0800

    Add authentication middleware and update routes for admin and auth callbacks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+import { clerkMiddleware } from "@clerk/express";
 import { PORT } from "../config/env.js";
 import { connectDB } from "./lib/db.js";
 
@@ -10,11 +11,14 @@ import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
 
+import { getAdmin } from "./controller/admin.controller.js";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json()); // to parse req.body
+app.use(clerkMiddleware()); // this will add auth to req obj
+
+app.get("/", getAdmin);
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoutes);
