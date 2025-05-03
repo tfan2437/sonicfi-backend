5d7128b135a93e15397743cd5117eaad78c81500
Ting Wei Fan <tfan2437@gmail.com>
2025-05-04 00:01:18 +0800

    Enhance package.json with seed scripts, add CORS middleware for cross-origin requests, and update user retrieval logic to fetch all users.

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,9 +3,11 @@ import { User } from "../models/user.model.js";
 export const getAllUser = async (req, res, next) => {
   try {
     const currentUserId = req.auth.userId;
-    const users = await User.find({
-      clerkId: { $ne: currentUserId },
-    });
+    // const users = await User.find({
+    //   clerkId: { $ne: currentUserId },
+    // });
+
+    const users = await User.find();
 
     res.status(200).json(users);
   } catch (error) {
